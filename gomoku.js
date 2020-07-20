414905406afe21749eee0947aa5c78a4ccc9d215
lakemaster <lakelord@jojobi.de>
2020-07-20 14:46:02 +0200

    add first very simple calculator

--- gomoku.js
+++ gomoku.js
@@ -9,12 +9,21 @@ window.addEventListener("load", () => {
 
 
     game.renderer.board.addEventListener("click", function(event) {
-        console.log(event);
         let pos = game.renderer.calcPosition(event);
-        console.log(pos);
 
-        if ( pos[0] > 0 && pos[1] > 0 ) {
+        if ( game.board.isValidAndFree(pos[0], pos[1]) ) {
             game.board.setStone(CrossState.black, pos[0], pos[1]);
+
+            if ( !game.board.gameOver(CrossState.black) ) {
+                pos = game.calculator.calculate(game.board.grid);
+                if ( game.board.isValidAndFree(pos[0], pos[1]) ) {
+                    game.board.setStone(CrossState.white, pos[0], pos[1]);
+                }
+            }
+            if ( game.board.gameOver(CrossState.white) || game.board.gameOver(CrossState.black) ) {
+                alert("Game over");
+                document.location.reload();
+            }
         }
     })
 
@@ -24,6 +33,7 @@ class Game {
     constructor(gridSize) {
         this.board = new Board(gridSize);
         this.renderer = new Renderer(gridSize);
+        this.calculator = new Calculator(gridSize);
     }
 
     start() {
@@ -44,16 +54,50 @@ class Board {
     }
 
     setStone(stone, x, y) {
-        if ( x <= 0 || x >= this.gridSize || y <= 0 || y >= this.gridSize) {
+        if ( !this.isValid(x, y) ) {
             alert("Error: (" + x + "," + y + ") out of range (" + this.gridSize + ")");
             return;
         }
-        if ( this.grid[x][y] != CrossState.free ) {
+        if ( ! this.isFree(x,y) ) {
             alert("Error: Position (" + x + "," + y + ") occupied");
             return;
         }
         this.grid[x][y] = stone;
         game.renderer.drawStone(stone, x, y);
+        console.log(this.grid);
+    }
+
+    isValid(x, y) {
+        return  x > 0 && x < this.gridSize && y > 0 && y < this.gridSize;
+    }
+
+    isFree(x, y) {
+        return this.grid[x][y] == CrossState.free;
+    }
+
+    isValidAndFree(x, y) {
+        return  this.isValid(x, y) && this.isFree(x, y);
+    }
+
+
+    // todo: is buggy - improve
+    gameOver(stone) {
+        let g = this.grid;
+
+        for ( let x = 5; x < this.gridSize-4; x++ ) {
+            for ( let y = 1; y < this.gridSize-4; y++ ) {
+                if ( g[x][y] == stone ) {
+                    if ( g[x+1][y] == stone && g[x+2][y] == stone && g[x+3][y] == stone && g[x+4][y] == stone 
+                        || g[x+1][y+1] == stone && g[x+2][y+2] == stone && g[x+3][y+3] == stone && g[x+4][y+4] == stone 
+                        || g[x][y+1] == stone && g[x][y+2] == stone && g[x][y+3] == stone && g[x][y+4] == stone 
+                        || g[x-1][y+1] == stone && g[x-2][y+2] == stone && g[x-3][y+3] == stone && g[x-4][y+4] == stone 
+                        || g[x-1][y] == stone && g[x-2][y] == stone && g[x-3][y] == stone && g[x-4][y] == stone ) {
+                            return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
 
@@ -111,6 +155,66 @@ class Renderer {
         }
 
         return( [Math.round(x/10), Math.round(y/10)] );
+    }
+}
+
+class Calculator {
+    constructor(gridSize) {
+        this.gridSize = gridSize;
+    }
+
+    calculate(grid) {
+        for ( let x = 1; x < this.gridSize; x++) {
+            for ( let y = 1; y < this.gridSize; y++) {
+                let res = this.testPosition(grid, x, y);
+                console.log("res: " + res);
+                if ( res >= 0 ) {
+                    console.log("calculated: " + x + ", " + y);
+                    return [x, y];
+                }
+            }
+        }
+        return [-1, -1];
+    }
+
+    testPosition(grid, x, y) {
+        if ( grid[x][y] != CrossState.free ) {
+            return -1;
+        }
+        console.log("Check: " + x + ","+ y);
+
+        let vN = this.validNeighbors(x, y);
+        console.log("valid neighbors: " + vN);
+        for ( let i = 0 ; i < vN.length; i++ ) {
+            console.log("Check VN: " + vN[i]);
+            if ( grid[vN[i][0]][vN[i][1]] == CrossState.black ) {
+                console.log("found neighbor: " + vN[i] + " of " + x + ", "+ y);
+                return 0;
+            }
+        }
+
+        return -1;
+    }
+
+    validNeighbors(x, y) {
+        let neighbors = [
+            [x-1, y-1],
+            [x, y-1],
+            [x+1, y-1],
+            [x-1, y],
+            [x+1, y],
+            [x-1, y+1],
+            [x, y+1],
+            [x+1, y+1]
+        ];
+
+        let validNeighbors = [];
+        neighbors.forEach((pos)=>{
+            if ( game.board.isValid(pos[0], pos[1]) ) {
+                    validNeighbors.push(pos);
+            }
+        });
+        return validNeighbors;
     }
 }
 
